a70e7dee8939df48e7816d07e4ea42ed9244882c
Michał Gawino <michal.gawino@gmail.com>
2025-06-12 20:31:58 +0200

    Added basic layout, login and registration components

--- src/main/shop-web/src/app/app.component.ts
+++ src/main/shop-web/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    NzBreadCrumbModule,
+    NzIconModule,
+    NzMenuModule,
+    NzLayoutModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'shop-web';
